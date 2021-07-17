2e59c1d37e19d3ca3b3f78f2af2ba7b9e13f72b2
Gabriel Sena <82296264+gabrielsenao@users.noreply.github.com>
2021-07-17 13:51:19 -0300

    Add files via upload

---
+++ impopar.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+
+Par ou Impar
+"""
+n=int(input("digite um numero = "))
+resto=n % 2 
+if (resto==0):
+    print("par")
+else: 
+    print("impar")
